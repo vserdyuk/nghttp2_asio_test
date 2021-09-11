c2b92df1526f8e5fc9865dcd8d3a8c0fab42881e
Vladimir Serdyuk <vser35@gmail.com>
2021-09-11 16:53:20 +0300

    add example project

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <nghttp2/asio_http2_server.h>
+
+#include <iostream>
+
+using namespace nghttp2::asio_http2;
+using namespace nghttp2::asio_http2::server;
+
+int main(int argc, char* argv[]) {
+	boost::system::error_code ec;
+	http2 server;
+
+	server.handle("/", [](const request& req, const response& res) {
+			res.write_head(200);
+			res.end("hello, world\n");
+		});
+
+	if (server.listen_and_serve(ec, "localhost", "3000")) {
+		std::cerr << "error: " << ec.message() << std::endl;
+	}
+}
